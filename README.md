549e72c1df5ca31d04477a15abada77f89c38a23
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-12-01 17:53:38 +0200

    Add image cards

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Gig.ly GUI mockup
 
 Simple GUI mockup for Software Product Management course
+This view is only for mobile so on desktop it will look horrible.
--- src/components/Gigs.tsx
+++ src/components/Gigs.tsx
@@ -1,5 +1,35 @@
+import "../gigs.css"
+import image from "../plumber.webp"
+
 function Gigs() {
-  
+  const gigs = [
+    "Plumber",
+    "Electrician",
+    "Carpenter",
+    "Dog walker",
+    "Babysitter",
+    "Cleaner",
+    "Barber",
+    "Hairdresser",
+    "Painter",
+    "Gardener",
+    "Tutor",
+    "Photographer",
+
+  ]
+
+  return (
+    <main>
+      <h2>What kind of help do you need?</h2>
+      <ul className="gigGrid">
+        {gigs.map((gig) => (
+          <li className="gig" key={gig}>
+            {gig}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
 
 export default Gigs
